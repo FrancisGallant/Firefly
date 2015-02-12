f67f8a487e234a9a893b177bda9df9d4096bf589
unknown <0833964@408W11.decinfo.cvm>
2015-02-12 16:58:19 -0500

    modifie le player encore

--- js/player.js
+++ js/player.js
@@ -9,6 +9,10 @@ function player (){
 	this.element.className("player");
 	this.element.style.left = "320px";
 	this.element.style.top = "387px";
+	this.maxSpeedX = 10;
+	this.speedX = 0;
+	this.velocityX = 0.5;
+
 
 	document.getElementById("body").appendChild(this.element);
 
@@ -16,6 +20,32 @@ function player (){
 }
 
 player.prototype.tick = function (){
+
+	if( leftPushed == true && this.speedX >= -this.maxSpeedX ){
+		this.speedX -= this.maxSpeedX;
+
+
+		
+	}
+
+	else if(leftPushed == false){
+
+		this.speedX = 0;
+	}
+	else if(rightPushed == true && this.speedX <= this.maxSpeedX){
+		this.speedX += this.maxSpeedX;
+	}
+
+	else if(rightPushed == false){
+		this.speedX = 0;
+	}
+
+	
+	left = this.element.offsetLeft;
+
+	this.element.style.left = left + this.speedX + "px";
+
+
 
 
 }
