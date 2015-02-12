ce946a025cb66066fe158fd92aa44e66c1fe7045
unknown <1148688@408W10.decinfo.cvm>
2015-02-12 16:15:27 -0500

    un peu etoile

--- js/etoile.js
+++ js/etoile.js
@@ -1,1 +1,17 @@
-etoile.js
+function Etoile(x,y){
+	this.x;
+	this.y;
+	this.speed = 3;
+	this.velocity = 0.3;
+	this.element = document.createElement("div");
+	this.element.className = "etoile";
+	this.element.style.left = this.x + "px";
+	this.element.style.top = this.y + "px";
+
+	document.getElementById("body").appendChild(this.element);
+
+}
+
+Etoile.prototype.tick = function(){
+
+}
---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+window.onload = init;
+
+function init(){
+	var bg = document.getElementById("body");
+	bg.style.backgroundImage = "url('firefly-splash.png')";
+}
